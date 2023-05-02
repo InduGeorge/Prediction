a7ea61624c37cfdb8fc59ae17d6718cceb4b8de5
Indu George <93480514+InduGeorge@users.noreply.github.com>
2023-05-02 10:50:38 +0530

    Update NB Classifier.py

--- NB Classifier.py
+++ NB Classifier.py
@@ -2,11 +2,8 @@ import pandas as pd
 
 bbc_text = pd.read_csv('bbc-text.txt')
 bbc_text=bbc_text.rename(columns = {'text': 'News_Headline'}, inplace = False)
-bbc_text.head()
 
 bbc_text.category = bbc_text.category.map({'tech':0, 'business':1, 'sport':2, 'entertainment':3, 'politics':4})
-
-
 
 from sklearn.model_selection import train_test_split
 X = bbc_text.News_Headline
@@ -27,15 +24,12 @@ X_transformed.toarray()
 X_test_transformed = vector.transform(X_test)
 
 
-
 from sklearn.naive_bayes import MultinomialNB
 naivebayes = MultinomialNB()
 naivebayes.fit(X_transformed, y_train)
 
 
 import streamlit as st
-
-
 
 st.header('Machine Prediction Demo')
 input = st.text_area("Please enter the text", value="")
